ff714676484d6ad0936f6314157be7dc9dc57548
xianghui.yang <xianghui.yang@datayes.com>
2020-02-24 18:42:05 +0800

    add folder

--- LeetCode-Swift/Easy/BinaryTreeLevelOrderTraversalII.swift
+++ LeetCode-Swift/Easy/BinaryTreeLevelOrderTraversalII.swift
@@ -38,3 +38,57 @@ class BinaryTreeLevelOrderTraversalII {
         print(root)
     }
 }
+
+
+
+
+class symmetricTreeNode {
+    
+    // 101 对称二叉树
+    func isSymmetric(_ root: TreeNode?) -> Bool {
+        return isMirror(root?.left, root?.right)
+    }
+    
+    func isMirror(_ left: TreeNode?, _ right: TreeNode?) -> Bool {
+        if left == nil && right == nil {
+            return true
+        }
+        
+        if left == nil || right == nil {
+            return false
+        }
+        
+        return left?.val == right?.val && isMirror(left?.left, right?.right) && isMirror(left?.right, right?.left)
+    }
+    
+    
+    // 671 二叉树第二小的节点
+
+    func findSecondMinimumValue(_ root: TreeNode?) -> Int {
+       
+        guard let root = root, let left = root.left, let right = root.right else {
+            return -1
+        }
+                
+        let minLeft = dfsMinimumValue(node: left)
+        let minRight = dfsMinimumValue(node: right)
+
+        if minLeft == root.val && minRight == root.val {
+            return -1
+        }else {
+            return min(minLeft, minRight)
+        }
+    }
+    
+    func dfsMinimumValue(node: TreeNode) -> Int {
+        if let left = node.left, let right = node.right {
+            return min(dfsMinimumValue(node: left), dfsMinimumValue(node: right))
+        }else {
+            return node.val
+        }
+    }
+    
+}
+
+
+
